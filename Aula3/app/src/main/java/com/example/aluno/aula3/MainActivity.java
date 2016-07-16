7bc09ce5e45d74bd68579b593dc4c31c6ab0c8cb
Desenvolvimento de Aplicativos com Android Studio <Desenvolvimento de Aplicativos com Android Studio>
2016-07-15 21:33:45 -0300

    Added spinner, action bar, menus.

--- Aula3/app/src/main/java/com/example/aluno/aula3/MainActivity.java
+++ Aula3/app/src/main/java/com/example/aluno/aula3/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.aluno.aula3;
 
 import android.app.Activity;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Aluno on 14/07/2016.
@@ -26,6 +36,56 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitle(R.string.app_name);
 
         setSupportActionBar(toolbar);
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
+
+        List<String> list = new ArrayList<String>();
+
+        list.add("Item 1");
+        list.add("Item 2");
+        list.add("Item 3");
+        list.add("Item 4");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_spinner_dropdown_item,
+                list);
+        mySpinner.setAdapter(adapter);
+
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        startActivity(new Intent(this,NewActivity.class));
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent( MainActivity.this,NewActivity.class));
+            }
+        });
+
+        Snackbar snackbar = Snackbar.make(toolbar, "Minha notificação", Snackbar.LENGTH_INDEFINITE);
+
+        snackbar.setAction("ABRIR", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "ABRIU!", Toast.LENGTH_LONG).show();
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this);
+                builder.setSmallIcon(R.mipmap.ic_launcher);
+                builder.setContentTitle("Meu Título");
+                builder.setContentText("Minha informacao");
+
+                NotificationManager nm =
+                        (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+                nm.notify(0,builder.build());
+
+            }
+        });
+
+        snackbar.show();
+
     }
 
 
---
+++ Aula3/app/src/main/java/com/example/aluno/aula3/NewActivity.java
@@ -0,0 +1,33 @@
+package com.example.aluno.aula3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+/**
+ * Created by Aluno on 15/07/2016.
+ */
+public class NewActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_new);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
